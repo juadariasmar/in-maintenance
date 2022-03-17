53c167e3b22f9f29da26769e2e8530b269599b75
juadariasmar <juadarismar@gmail.com>
2022-03-16 19:03:45 -0500

    In maintenance React page

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import "./main.css";
+
+const main = () => {
+  return (
+    <main className="i">
+      <h2 className="i-title-h">Hello</h2>
+      <h2 className="i-title2">
+        My name is <h1 className="i-title">Juan Arias</h1>
+      </h2>
+      <section className="i-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-paint"
+          width="100"
+          height="100"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#13315C"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="5" y="3" width="14" height="6" rx="2" />
+          <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" />
+          <rect x="10" y="15" width="4" height="6" rx="1" />
+        </svg>
+        <article>
+          <h2 className="i-center-item">Comic Soon</h2>
+          <h3 className="i-center-item">In Maintenance</h3>
+        </article>
+        <p>
+          Visit my{" "}
+          <a href="https://github.com/juadariasmar" className="i-link">
+            GitHub
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-brand-github"
+              width="60"
+              height="60"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#000000"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+            </svg>
+          </a>
+        </p>
+      </section>
+      <footer className="i-footer">
+        <a
+          href="https://www.linkedin.com/in/juan-arias14/"
+          className="i-linkedin"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-brand-linkedin"
+            width="60"
+            height="60"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#13315C"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <rect x="4" y="4" width="16" height="16" rx="2" />
+            <line x1="8" y1="11" x2="8" y2="16" />
+            <line x1="8" y1="8" x2="8" y2="8.01" />
+            <line x1="12" y1="16" x2="12" y2="11" />
+            <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+          </svg>
+        </a>
+        <a href="https://wa.me/573013643804" className="i-whatsapp">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-brand-whatsapp"
+            width="60"
+            height="60"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#13315C"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+            <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+          </svg>
+        </a>
+      </footer>
+    </main>
+  );
+};
+
+export default main;
